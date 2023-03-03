e11de239e9a6f093346a31c83b09d49996d73e8c
Roshan Kumar <roshaen09@gmail.com>
2023-03-03 10:53:26 +0530

    added hospital data

---
+++ lib/utils/hospital_data.dart
@@ -0,0 +1,36 @@
+final List hospitalData = [
+  {
+    "hospitalName": "ECHS Polyclinic Salt Lake",
+    "address": "EM Block, Sector V, Bidhannagar, Kolkata, West Bengal 700091",
+    "distance": "2.1",
+    "url": "https://goo.gl/maps/tc1i49JPdjUicaDTA",
+  },
+  {
+    "hospitalName": "Care Hospital",
+    "address":
+        "Fd216, Flat-1, Sector-III, Salt Lake, BN Block, Sector V, Bidhannagar, Kolkata, West Bengal 700c091",
+    "distance": "1.7",
+    "url": "https://goo.gl/maps/azDR4rvrpCU6CC5A6",
+  },
+  {
+    "hospitalName": "Matri Sadan Bidhannagar Municipal Hospital",
+    "address":
+        "Salt Lake, Main Lane 1, EE-55A, 3rd Ave, EE Block, Sector II, Bidhannagar, Kolkata, West Bengal 700091",
+    "distance": "1.9",
+    "url": "https://goo.gl/maps/z4WvvKSnMZ8gUhEF8",
+  },
+  {
+    "hospitalName": "Calcutta Heart Clinic & Hospital",
+    "address":
+        "3, 1st Cross Rd, HC Block, Sector III, Bidhannagar, Kolkata, West Bengal 700106",
+    "distance": "2.2",
+    "url": "https://goo.gl/maps/5oaCfTWLqx1tPmW49",
+  },
+  {
+    "hospitalName": "AMRI Hospital - Salt Lake",
+    "address":
+        "17,Lane, Central Park Road,Stadium Entrance Road,, 16, Broadway Rd, opposite salt lake, JC Block, Sector III, Bidhannagar, Kolkata, West Bengal 700098",
+    "distance": "3.8",
+    "url": "https://goo.gl/maps/U24q2b3ZfhFU3kfa7",
+  }
+];
